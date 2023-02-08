9421e1e17ba50dfeff9bc9bad95149aa32893482
gajua <sewon0325@naver.com>
2023-02-08 15:14:46 +0900

    header markup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import Router from "./pages/Routes/Router";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+}
+`;
+
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Router />
     </>
   );
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -5,22 +5,66 @@ import SearchInp from "../SearchInp/SearchInp";
 const LogoImg = styled.img`
   width: 124px;
   height: 38px;
-  border: 1px solid #000;
   margin: 0 30px;
 `;
 const HomeHeader = styled.header`
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.1);
+  position: relative;
+  padding: 22px 320px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  div {
+    display: flex;
+    align-items: center;
+    display: flex;
+  }
+`;
+
+const HeaderNav = styled.nav`
+  ul {
+    display: flex;
+    gap: 26px;
+    li {
+      width: 56px;
+      height: 50px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      img{
+        margin-bottom: 5px;
+      }
+      span {
+        font-size: 12px;
+        color: #767676;
+      }
+    }
+  }
 `;
 
 export default function Header() {
   return (
     <HomeHeader>
-      <h1>
-        <LogoImg src="" alt="호두마켓 로고" />
-      </h1>
-      <SearchInp />
-      <div></div>
+      <div>
+        <h1>
+          <LogoImg src="assets/Logo-hodu.png" alt="호두마켓 로고" />
+        </h1>
+        <SearchInp />
+      </div>
+      <HeaderNav>
+        <ul>
+          <li>
+            <img src="assets/icon-shopping-cart.svg" alt="장바구니" />
+            <span>장바구니</span>
+          </li>
+          <li>
+            <img src="assets/icon-user.svg" alt="로그인" />
+            <span>로그인</span>
+          </li>
+        </ul>
+      </HeaderNav>
     </HomeHeader>
   );
 }
